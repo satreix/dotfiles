4598c9e4063be4b9e3e73d54068beecf578159d5
satreix <satreix@gmail.com>
2016-02-11 17:45:38 +0100

    install: add setup script
    fix #6

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install oh-my-zsh
+
+if [ ! -x /usr/bin/zsh ]; then
+    omzsh_url='https://github.com/robbyrussell/oh-my-zsh'
+    echo "Found zsh, installing oh-my-zsh: $omzsh_url"
+    sh -c "$(curl -fsSL ${omzsh_url}/master/tools/install.sh)"
+elif
+    echo "Didn't find zsh, skipping oh-my-zsh installation"
+fi
